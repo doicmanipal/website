261c69a3a92816c5c95a76c9a388b3d5389f62f2
Arunabha-NOTE <arunabham2017@gmail.com>
2024-08-29 23:56:46 +0530

    final commit hopefully v2

--- src/components/internshipAccordion.jsx
+++ src/components/internshipAccordion.jsx
@@ -21,7 +21,7 @@ const InternshipAccordion = () => {
             {internships.map((internship) => (
                 <div key={internship.id}>
                     <div className="join join-vertical w-full">
-                        <div className="collapse collapse-arrow join-item border-base-300 border">
+                        <div className="collapse collapse-arrow join-item border-base-300 bg-white border">
                             <input type="radio" name="my-accordion-4" defaultChecked/>
                             <div className="collapse-title text-xl font-medium">
                                 <PrismicRichText
