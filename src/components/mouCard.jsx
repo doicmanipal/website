ec431e3ec75fde76c2f83225aa8773d6d33a9a2e
someonetookmycode <anvit.dd@gmail.com>
2024-09-20 01:22:16 +0530

    converted mou card to accordian

--- src/components/mouCard.jsx
+++ src/components/mouCard.jsx
@@ -1,46 +1,87 @@
-import {PrismicRichText, useAllPrismicDocumentsByType} from "@prismicio/react";
-
+import {
+  PrismicRichText,
+  useAllPrismicDocumentsByType,
+} from "@prismicio/react";
 
 const MouCard = () => {
-    const [mou, { state, error }] = useAllPrismicDocumentsByType('mou');
+  const [mou, { state, error }] = useAllPrismicDocumentsByType("mou");
 
-    if (state === 'loading') {
-        return <span className="loading mx-auto flex loading-ring loading-lg"></span>;
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    if (!mou || mou.length === 0) {
-        return <div className='hidden'>No mous's found.</div>;
-    }
+  if (state === "loading") {
     return (
-        <>
-        {mou.map((mous) => (
-            <div className='hover:-translate-y-3 translate-y-0  duration-300' key={mous.id}>
-                <div className="card bg-base-100 min-w-80 min-h-[20.5rem] shadow-xl">
-                    <div className="card-body">
-                        <div className="">
-                            <PrismicRichText
-                                field={mous.data.mouname}
-                                components={{
-                                    heading1: ({children}) => <h2 className='card-title'>{children}</h2>,
-                                }}
-                            />
-                        </div>
-                        <p><span className='font-bold'>Qs: </span>{mous.data.qs}</p>
-                        <p><span className='font-bold'>Department: </span>{mous.data.department}</p>
-                        <p><span className='font-bold text-clip text-nowrap '>Muj-nodal: </span>{mous.data.nodal}</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary"><a href={mous.data.link}>MoU</a></button>
-                        </div>
-                    </div>
-                </div>
+      <span className="loading mx-auto flex loading-ring loading-lg"></span>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!mou || mou.length === 0) {
+    return <div className="hidden">No mous's found.</div>;
+  }
+  return (
+    <>
+      {mou.map((mous) => (
+        <div className="collapse collapse-arrow bg-base-200" key={mous.id}>
+          <input type="radio" name="my-accordion-1" defaultChecked />
+          <div className="collapse-title text-xl font-medium">
+            <PrismicRichText
+              field={mous.data.mouname}
+              components={{
+                heading1: ({ children }) => (
+                  <h2 className="card-title">{children}</h2>
+                ),
+              }}
+            />{" "}
+          </div>
+          <div className="collapse-content space-y-2">
+            <div>
+              <p>
+                <span className="font-bold">Qs: </span>
+                {mous.data.qs}
+              </p>
+              <p>
+                <span className="font-bold">Department: </span>
+                {mous.data.department}
+              </p>
+              <p>
+                <span className="font-bold text-clip text-nowrap ">
+                  Muj-nodal:{" "}
+                </span>
+                {mous.data.nodal}
+              </p>
             </div>
-        ))}
-        </>
-);
+            <div>
+              {" "}
+              <button className="btn btn-primary">
+                <a href={mous.data.link}>MoU</a>
+              </button>{" "}
+            </div>
+          </div>
+        </div>
+        // <div className='hover:-translate-y-3 translate-y-0  duration-300' key={mous.id}>
+        //     <div className="card bg-base-100 min-w-80 min-h-[20.5rem] shadow-xl">
+        //         <div className="card-body">
+        //             <div className="">
+        //                 <PrismicRichText
+        //                     field={mous.data.mouname}
+        //                     components={{
+        //                         heading1: ({children}) => <h2 className='card-title'>{children}</h2>,
+        //                     }}
+        //                 />
+        //             </div>
+        //             <p><span className='font-bold'>Qs: </span>{mous.data.qs}</p>
+        //             <p><span className='font-bold'>Department: </span>{mous.data.department}</p>
+        //             <p><span className='font-bold text-clip text-nowrap '>Muj-nodal: </span>{mous.data.nodal}</p>
+        //             <div className="card-actions justify-end">
+        //                 <button className="btn btn-primary"><a href={mous.data.link}>MoU</a></button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+      ))}
+    </>
+  );
 };
 
 export default MouCard;
