a44bce974d8dcb668720553cde7481b98cf0a919
Arunabha-NOTE <arunabham2017@gmail.com>
2024-10-08 12:00:14 +0530

    added apply button

--- src/components/eventCard.jsx
+++ src/components/eventCard.jsx
@@ -56,12 +56,12 @@ const EventCard = ({ activeTab }) => {
             <div className="card-body">
               <div>
                 <PrismicRichText
-                  field={event.data.title}
-                  components={{
-                    heading1: ({ children }) => (
-                      <h2 className="card-title">{children}</h2>
-                    ),
-                  }}
+                    field={event.data.title}
+                    components={{
+                      heading1: ({children}) => (
+                          <h2 className="card-title">{children}</h2>
+                      ),
+                    }}
                 />
               </div>
               <p>
@@ -89,8 +89,17 @@ const EventCard = ({ activeTab }) => {
                 <span className="font-bold">Registration status:</span>{" "}
                 {event.data.status}
               </p>
+              <a href={event.data.brochure} target="_blank">
+                <button className="btn btn-primary w-28">Brochure</button>
+              </a>
               <a href={event.data.link} target="_blank">
-                <button className="btn btn-primary">Brochure</button>
+                <button className="btn btn-primary w-28">Apply
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                       viewBox="0 0 512 512" height="20">
+                    <path
+                        d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/>
+                  </svg>
+                </button>
               </a>
             </div>
           </div>
